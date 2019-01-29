e799a611de02d54f964d9c22c83c57240e24821e
D3STRON <ghoshanurag99@gmail.com>
2019-01-29 10:55:30 +0530

    peer routing

--- peer.js
+++ peer.js
@@ -2,14 +2,20 @@ var topology = require('fully-connected-topology')
 var jsonStream = require('duplex-json-stream')
 var streamSet = require('stream-set')
 var register = require('register-multicast-dns')
-
+var toPort = require('hash-to-port')
+var id = Math.random()
+var seq = 0
+var logs = {}
 
 var me = process.argv[2]
 var peers = process.argv.slice(3)
 
 
+function toAddress(name){
+    return 'localhost:' + toPort(name)
+}
 
-var swarm = topology(me,peers)
+var swarm = topology(toAddress(me), peers.map(toAddress))
 var streams = streamSet()
 
 register(me)
@@ -21,7 +27,8 @@ swarm.on('connection',function(fellowPeer){
     fellowPeer = jsonStream(fellowPeer)
     streams.add(fellowPeer)
     fellowPeer.on('data',function(data){
-       
+        if(logs[data.log]>= data.seq) return
+        logs[data.log] = data.seq
         console.log(data.username +'~ '+ data.message)
         streams.forEach(function(otherFriend){
             otherFriend.write(data)
